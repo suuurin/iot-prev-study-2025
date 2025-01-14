069fc791a9213ee0f1b7f2f7c9828a2e07027ce7
suuurin <z11673@naver.com>
2025-01-14 17:11:19 +0900

    마지막 정리

--- day01/py01_first.py
+++ day01/py01_first.py
@@ -1,4 +1,4 @@
-# IoT과정 첫 번째 프로그램 파일
+# IoT과정 첫 번째 프로그램 파일 - 주석
 
-if __name__ == '__main__':
-    print("Hello, Python!")
+if __name__ == '__main__': # 조건문, __name__과 '__main__'이 같다면.
+    print("Hello, Python!") #print() 함수로 실행해라.
